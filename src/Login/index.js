e8750f05abf3349743bbe4f6124923af9007628a
Lucas Marques <lucas.inst@outlook.com.br>
2019-10-30 20:30:54 -0300

    Login v. Alpha

---
+++ src/Login/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './index.css';
+import img_placeholder from "../public/placeholder-img.jpg";
+
+function Index() {
+    return(
+        <div id="login">
+            <div id="logo-login">
+                <img src={img_placeholder} alt=""/>
+            </div>
+
+            <div id="form-login">
+                <form>
+                    <img src={img_placeholder} alt=""/>
+                    
+                    <input 
+                        type="text" 
+                        placeholder="Login" />
+                    <input 
+                        type="password" 
+                        placeholder="Senha" />
+                    
+                    <input 
+                        type="submit" 
+                        value="ENTRAR"/>
+                </form>                
+            </div>
+        </div>
+    );
+}
+
+export default Index;
