97daad23dc5a399b4c88da1711b4d17535563257
Gabriel Laurencio <laurencio.arkauss@gmail.com>
2020-01-28 21:46:51 -0300

    fix : a pasta de public não veio junta

---
+++ src/public/header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import'./header.css';
+import {Link} from 'react-router-dom';
+
+import icon_maquina from '../public/icons/icon_maquina.png';
+import icon_sair from '../public/icons/icon_sair.png';
+
+class Header extends Component{
+    sair = () =>{
+        sessionStorage.clear();
+    }
+    render(){
+        return(
+            <div id="header">
+                <Link to='/Menu'>
+                    <img src={icon_maquina} alt=" " ></img>
+                </Link>
+
+                <h1>{this.props.name.toUpperCase()}</h1>
+                <Link to='/'><img src={icon_sair} alt=" " onClick={this.sair} /></Link>
+            </div>
+        );
+    }
+}
+
+export default Header;
