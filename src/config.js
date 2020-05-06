48a63562dc5150477f81dc84f81bfd2135d23a6a
Lucas M <lucas.inst@outlook.com.br>
2020-05-06 20:55:33 -0300

    fix: config production

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const DEVELOPMENT = "http://192.168.0.28:3000";
+const DEVELOPMENT = "http://localhost:3000";
 const PRODUCTION = "https://bubble-system-back.herokuapp.com";
 
 const CONNECT = PRODUCTION;
